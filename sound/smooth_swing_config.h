3406ba2057197417b325752fc58b7022134e0baa
SA-22C-smoothswing <49962985+SA-22C-smoothswing@users.noreply.github.com>
2019-07-15 20:26:34 -0600

    defaults - 450 accent swing 700 accent slash

--- sound/smooth_swing_config.h
+++ sound/smooth_swing_config.h
@@ -12,8 +12,8 @@ public:
     CONFIG_VARIABLE(Transition1Degrees, 45.0f);
     CONFIG_VARIABLE(Transition2Degrees, 160.0f);
     CONFIG_VARIABLE(MaxSwingVolume, 3.0f);
-    CONFIG_VARIABLE(AccentSwingSpeedThreshold, 0.0f);
-    CONFIG_VARIABLE(AccentSlashAccelerationThreshold, 0.0f);
+    CONFIG_VARIABLE(AccentSwingSpeedThreshold, 450.0f);
+    CONFIG_VARIABLE(AccentSlashAccelerationThreshold, 700.0f);
   };
 
   int  Version;
